ccb8ca020a3febc088251ef06dc8d391d8d95aff
Michael Mulich <michael.mulich@gmail.com>
2012-07-18 10:29:57 -0400

    Refactor the path inspection into a function

--- js/cnx-module.js
+++ js/cnx-module.js
@@ -1,28 +1,37 @@
 /* Module viewing code
 
 */
+var baseURL = "http://cnx.org",
+    moduleUndefined = false;
+
+
+getModulePartsFromPath = function(path) {
+    result = {id: undefined, version: 'latest'}
+    path = path.split("/");
+    if (path[1] !== "content") { return result; }
+    module = path[path.length-1];
+    splitInfo = module.split("@");
+    if (splitInfo.length == 2) {
+        result.id = splitInfo[0];
+        result.version = splitInfo[1];
+    } else {
+        result.id = module;
+    }
+    return result;
+}
 
 // On document load fill the page with the module content.
 $(document).ready(function() {
-    var url = "http://cnx.org",
-        moduleUndefined = false,
-        modulePath = window.location.hash.slice("#!".length),
+    var modulePath = window.location.hash.slice("#!".length),
         moduleId = undefined,
-        moduleVersion = 'latest',
-        splitModulePath = modulePath.split("/");
-
+        moduleVersion = undefined,
+        url = "";
     console.log("module path: " + modulePath);
-    if (modulePath === "" || splitModulePath[1] === "") {
+    module = getModulePartsFromPath(modulePath);
+    if (module.id === undefined) {
         moduleUndefined = true;
     } else {
-        //url = url + modulePath;
-        moduleId = splitModulePath[splitModulePath.length-1]
-        if (moduleId.split("@").length == 2) {
-            splitInfo = moduleId.split("@");
-            moduleId = splitInfo[0];
-            moduleVersion = splitInfo[1];
-        }
-        url = url + "/content/" + moduleId + "/" + moduleVersion;
+        url = baseURL + "/content/" + module.id + "/" + module.version;
     }
     console.log(url);
 
